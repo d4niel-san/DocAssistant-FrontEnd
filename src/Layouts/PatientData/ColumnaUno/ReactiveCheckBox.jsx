53b0813e0971d86a8028b9c6d27e14680ea0f4e5
Daniel <dmermoz@gmail.com>
2022-09-09 21:28:29 +0200

    funciona el reactivecheckbox

--- src/Layouts/PatientData/ColumnaUno/ReactiveCheckBox.jsx
+++ src/Layouts/PatientData/ColumnaUno/ReactiveCheckBox.jsx
@@ -1,28 +1,26 @@
 import { Checkbox } from "@mui/material";
 import { useContext } from "react";
-import { useState } from "react";
 import { ApiContext } from "../../../context/apiContext";
 
 export const ReactiveCheckBox = (props) => {
   const { payed, Id } = props;
 
-  const [disabler, setDisabler] = useState(true);
-  const { consultasImpagas } = useContext(ApiContext);
+  const { consultasImpagas, setConsultasImpagas } = useContext(ApiContext);
 
   //console.log("elemento: ",date, amount, payed, Id)
 
   const handleClick = () => {
-    setDisabler(!disabler);
     const auxConsulta = consultasImpagas.map((e) => {
       if (e.Id === Id) {
         return { ...e, payed: !e.payed };
       }
       return e;
     });
+    setConsultasImpagas(auxConsulta)
     console.log(auxConsulta);
   };
 
   return (
-    <Checkbox checked={!disabler} onClick={handleClick} color={"pinkMain"} />
+    <Checkbox checked={payed} onClick={handleClick} color={"pinkMain"} />
   );
 };
